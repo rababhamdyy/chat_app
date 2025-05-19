c85f5b4235f7bc5e48c7f559c75ec25e0af74c40
rabab_hamdyy <rababhamdy38@gmail.com>
2025-05-19 22:43:13 +0300

    first commit

---
+++ lib/views/chat_view.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class ChatView extends StatelessWidget {
+  const ChatView({super.key});
+
+  Widget messageBubble() {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.grey[400],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Text(
+          "Chat First Message",
+          style: TextStyle(color: Colors.black, fontSize: 20),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreen[900],
+        title: const Text("Chat App", style: TextStyle(color: Colors.white)),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.logout, color: Colors.white),
+          ),
+        ],
+      ),
+      // Chat Messsage List and Input Field
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 2,
+              itemBuilder: (context, index) {
+                return messageBubble();
+              },
+            ),
+          ),
+          // Message Input Field
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Type a message",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide(color: Colors.lightGreen[900]!),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide(color: Colors.lightGreen[900]!),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.send, color: Colors.lightGreen[900]),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
